0dc2a58bf601902bfb54a68d30d155c790a070a4
mattchley <mattchley@gmail.com>
2019-12-31 14:46:03 -0800

    connection,DB,ORMinit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western', true);
