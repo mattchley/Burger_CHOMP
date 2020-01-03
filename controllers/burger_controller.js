59392648047526420ed530695f4ea91b31609963
mattchley <mattchley@gmail.com>
2020-01-03 12:17:03 -0800

    postFunct

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burgers.js) to use its database functions.
-var burgers = require("../models/burgers.js");
+var burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
     var newDevoured = $(this).data("newDevoured");
 
@@ -26,7 +26,7 @@ $(function() {
     event.preventDefault();
 
     var newBurger = {
-      name: $("#burger").val().trim(),
+      name: $("#ca").val().trim(),
       devoured: $("[name=devoured]:checked").val().trim()
     };
 
@@ -37,21 +37,6 @@ $(function() {
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
         // Reload the page to get the updated list
         location.reload();
       }
